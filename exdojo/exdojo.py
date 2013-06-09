1dafbfa70cad09d86bfbb3d15782bda419cbd45a
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2013-06-09 20:37:51 -0300

    Using 4 spaces indent

--- exdojo/exdojo.py
+++ exdojo/exdojo.py
@@ -1,5 +1,5 @@
 def factorial(n):
-	if n == 0:
-		return 1
-	else:
-		return n * factorial(n - 1)
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
--- exdojo/test_exdojo.py
+++ exdojo/test_exdojo.py
@@ -4,17 +4,17 @@ from exdojo import factorial
 
 class TestFactorial(unittest.TestCase):
 
-	def test_factorial_0(self):
-		self.assertEqual(factorial(0), 1)
+    def test_factorial_0(self):
+        self.assertEqual(factorial(0), 1)
 
-	def test_factorial_1(self):
-		self.assertEqual(factorial(1), 1)
+    def test_factorial_1(self):
+        self.assertEqual(factorial(1), 1)
 
-	def test_factorial_2(self):
-		self.assertEqual(factorial(2), 2)
+    def test_factorial_2(self):
+        self.assertEqual(factorial(2), 2)
 
-	def test_factorial_3(self):
-		self.assertEqual(factorial(3), 6)
+    def test_factorial_3(self):
+        self.assertEqual(factorial(3), 6)
 
-	def test_factorial_4(self):
-		self.assertEqual(factorial(4), 24)
+    def test_factorial_4(self):
+        self.assertEqual(factorial(4), 24)
