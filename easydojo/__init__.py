b6085fdb8de3dc410f399b8339c848726baf86b0
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2013-06-14 12:38:41 -0300

    Fix: Updated MANIFEST with templates and static of panel

--- easydojo/__init__.py
+++ easydojo/__init__.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 __title__ = 'easydojo'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __author__ = 'Fábio Cerqueira'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 Fábio Cerqueira'
