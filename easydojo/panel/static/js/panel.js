a4b8d4cbdf6476df59548d4efd2a12b3f0b93ca4
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2013-06-16 21:57:00 -0300

    improved visual panel

--- easydojo/panel/static/js/panel.js
+++ easydojo/panel/static/js/panel.js
@@ -1,13 +1,21 @@
+counter_start = function () {
+    $('#counter').html("");
+    $('#counter').countdown({
+        image: '/static/img/digits.png',
+        startTime: '05:00',
+        timerEnd: counter_start,
+        format: 'mm:ss'
+    });
+}
 $(document).ready(function () {
+    counter_start();
     var ws = new WebSocket("ws://" + document.location.host + "/ws");
     ws.onmessage = function(evt) {
         if (evt.data == 'S') {
-            $('#red').attr('class', 'off');
-            $('#green').attr('class', 'green');
+            $('#signal').attr('class', 'green');
         }
         else if (evt.data == 'E') {
-            $('#red').attr('class', 'red');
-            $('#green').attr('class', 'off');
+            $('#signal').attr('class', 'red');
         }
     };
 });
